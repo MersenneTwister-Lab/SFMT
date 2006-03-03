81f09e1e3bf87f61e9d138c485d40f67381db001
saito <saito@5d39f105-0b1f-0410-87d7-cf9e5e27ca8e>
2006-03-03 02:09:55 +0000

    morinig commit
    git-svn-id: file:///Users/saito/svn-repos/sfmt/trunk@122 5d39f105-0b1f-0410-87d7-cf9e5e27ca8e

---
+++ src/sfmt.h
@@ -0,0 +1,21 @@
+/* Simple and Fast MT 2006/1/21 */
+#ifndef __SFMT_H__
+#define __SFMT_H__
+
+#include <stdint.h>
+#include <stdio.h>
+
+void setup_param(unsigned int p1, unsigned int p2, unsigned int p3, 
+		 unsigned int p4, unsigned int p5, unsigned int p6,
+		 unsigned int p7, unsigned int p8, unsigned int p9,
+		 unsigned int p10, unsigned int p11, unsigned int p12,
+		 unsigned int p13);
+unsigned int get_rnd_maxdegree(void);
+unsigned int get_rnd_mexp(void);
+void print_param(FILE *fp);
+void print_param2(FILE *fp);
+
+void init_gen_rand(uint32_t seed);
+uint32_t gen_rand(void);
+
+#endif
