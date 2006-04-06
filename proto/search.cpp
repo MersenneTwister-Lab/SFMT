5c03b4aa77197f46604190143a29839f912f8125
saito <saito@5d39f105-0b1f-0410-87d7-cf9e5e27ca8e>
2006-04-06 01:44:37 +0000

    new version
    git-svn-id: file:///Users/saito/svn-repos/sfmt/trunk@143 5d39f105-0b1f-0410-87d7-cf9e5e27ca8e

--- proto/search.cpp
+++ proto/search.cpp
@@ -14,91 +14,12 @@ extern "C" {
 #include <NTL/GF2X.h>
 #include <NTL/vec_GF2.h>
 
-NTL_CLIENT
+NTL_CLIENT;
 
-int non_reducible(GF2X& fpoly, int degree);
 void search(unsigned int n);
 
 static unsigned long long all_count = 0;
-static unsigned long long pass1_count = 0;
-static unsigned long long pass2_count = 0;
-static unsigned long long pass3_count = 0;
-static unsigned long long pass4_count = 0;
 int limit;
-
-int non_reducible(GF2X& fpoly, int degree) {
-    GF2X t2m = GF2X(2, 1);
-    GF2X t1 = GF2X(1, 1);
-    GF2X t;
-    GF2X alpha;
-    int m;
-    int count;
-
-    DPRINT("degree = %u", degree);
-    if (deg(fpoly) < degree) {
-	DPRINT("degree = %u", deg(fpoly));
-	return 0;
-    }
-    count = 1;
-    t = t1;
-    t += t2m;
-  
-    for (m = 1; deg(fpoly) > degree; m++) {
-	for(;;) {
-	    GCD(alpha, fpoly, t);
-	    if (IsOne(alpha)) {
-		break;
-	    }
-	    fpoly /= alpha;
-	    if (deg(fpoly) < degree) {
-		DPRINT("degree = %u", deg(fpoly));
-		return 0;
-	    }
-	}
-	if ((deg(fpoly) > degree) && (deg(fpoly) <= degree + m)) {
-	    DPRINT("deg(fpoly) = %u", deg(fpoly));
-	    return 0;
-	}
-	t2m *= t2m;
-	count++;
-	t2m %= fpoly;
-	add(t, t2m, t1);
-    }
-    if (deg(fpoly) != degree) {
-	DPRINT("degree = %u\n", degree);
-	return 0;
-    }
-    pass1_count++;
-    for (; m <= limit; m++) {
-	for(;;) {
-	    GCD(alpha, fpoly, t);
-	    if (IsOne(alpha)) {
-		break;
-	    }
-	    fpoly /= alpha;
-	    if (deg(fpoly) < degree) {
-		return 0;
-	    }
-	}
-	t2m *= t2m;
-	count++;
-	t2m %= fpoly;
-	add(t, t2m, t1);
-    }
-    pass2_count++;
-    for (;m < degree; m++) {
-	t2m *= t2m;
-	t2m %= fpoly;
-	count++;
-    }
-    add(t, t1, t2m);
-    if (deg(t) == -1) {
-	pass3_count++;
-	return 1;
-    } else {
-	return 0;
-    }
-}
 
 void generating_polynomial(vec_GF2& vec, unsigned int bitpos, 
 			   unsigned int maxdegree)
@@ -181,7 +102,6 @@ void search(unsigned int n) {
 	    }
 	}
 	if (bmOk) {
-	    pass4_count++;
 	    printf("----------\n");
 	    printf("succ = %u\n", ++succ);
 	    printf("deg = %ld\n", deg(minpoly));
@@ -194,18 +114,10 @@ void search(unsigned int n) {
 	}
 	if (all_count % 10000 == 0) {
 	    printf("count = %llu\n", all_count);
-	    printf("pass1 = %llu\n", pass1_count);
-	    printf("pass2 = %llu\n", pass2_count);
-	    printf("pass3 = %llu\n", pass3_count);
-	    printf("pass4 = %llu\n", pass4_count);
 	    fflush(stdout);
 	}
     }
     printf("count = %llu\n", all_count);
-    printf("pass1 = %llu\n", pass1_count);
-    printf("pass2 = %llu\n", pass2_count);
-    printf("pass3 = %llu\n", pass3_count);
-    printf("pass4 = %llu\n", pass4_count);
     fflush(stdout);
 }
 
