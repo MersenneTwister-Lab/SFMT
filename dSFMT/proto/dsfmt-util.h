20fb1b14752f7c4eadca90037ce4d4b20a9a443e
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2012-04-11 14:42:00 +0900

    move dSFMT to other repo

--- dSFMT/proto/dsfmt-util.h
+++
@@ -1,9 +0,0 @@
-extern "C" {
-  #include "dsfmt-st.h"
-}
-#include <NTL/vec_GF2.h>
-NTL_CLIENT;
-
-void generating_polynomial104(dsfmt_t *dsfmt, vec_GF2& vec, 
-			      unsigned int bitpos, 
-			      unsigned int maxdegree);
--- dSFMT/release/make-out.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-exps="607 1279 2281 4423 11213 19937 44497 86243 132049 216091"
-for mexp in $exps; do
-    gcc -DSFMT_MEXP=${mexp} -DMAIN -o dSFMT-ref-M${mexp} dSFMT-ref.c 
-    ./dSFMT-ref-M${mexp} > dSFMT.${mexp}.out.txt
-    ./dSFMT-ref-M${mexp} -ar > dSFMT.${mexp}.ar-out.txt
-done
-exit 0
