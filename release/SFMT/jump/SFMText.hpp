036eb627a429e524f7aea35d258501c02e7b02e7
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2012-04-16 15:59:08 +0900

    calculate O.K.

---
+++ release/SFMT/jump/SFMText.hpp
@@ -0,0 +1,236 @@
+#ifndef SFMTEXT_HPP
+#define SFMTEXT_HPP
+/**
+ * @file SFMText.hpp
+ *
+ * @brief exteted SFMT class for calculating characteristic polynomial.
+ *
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (The University of Tokyo)
+ *
+ * Copyright (C) 2012 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and The University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+#include <stdexcept>
+#include <stdint.h>
+#include <inttypes.h>
+
+namespace sfmt {
+    using namespace std;
+
+    struct w128_t {
+	uint32_t u[4];
+    };
+
+    class SFMText {
+    public:
+	/**
+	 * Constructor from parameters.
+	 */
+	SFMText(int mexp, int sl1, int sl2, int sr1, int sr2, int pos1,
+		uint32_t mask[4], uint32_t parity[4]) {
+		this->mexp = mexp;
+		this->sl1 = sl1;
+		this->sl2 = sl2;
+		this->sr1 = sr1;
+		this->sr2 = sr2;
+		this->pos1 = pos1;
+		this->mask1 = mask[0];
+		this->mask2 = mask[1];
+		this->mask3 = mask[2];
+		this->mask4 = mask[3];
+		this->parity[0] = parity[0];
+		this->parity[1] = parity[1];
+		this->parity[2] = parity[2];
+		this->parity[3] = parity[3];
+		this->size = mexp / 128 + 1;
+		this->maxdegree = size * 128;
+		this->state = new w128_t[size];
+		this->index = 0;
+		this->idx = 0;
+		this->bitpos = 0;
+	}
+
+	/**
+	 * release internal state.
+	 */
+	~SFMText() {
+	    delete[] state;
+	}
+
+	int get_mexp() {
+	    return mexp;
+	}
+
+	int get_maxdegree() {
+	    return maxdegree;
+	}
+
+	void init_basis() {
+	    if (bitpos >= maxdegree) {
+		throw new logic_error("exceed max degree");
+	    }
+	    int i, j, k;
+	    uint32_t mask;
+	    k = bitpos % 32;
+	    j = (bitpos / 32) % 4;
+	    i = bitpos / 128;
+	    memset(state, 0, sizeof(w128_t) * size);
+	    mask = 1 << k;
+	    state[i].u[j] = mask;
+	    bitpos++;
+	}
+
+	/**
+	 * generate next 128 bits.
+	 * @returns 128-bit number.
+	 */
+	w128_t next() {
+	    do_recursion();
+	    w128_t result = state[index];
+	    index = (index + 1) % size;
+	    return result;
+	}
+
+	int next(uint32_t mask[4]) {
+	    w128_t num = next();
+	    for (int i = 0; i < 4; i++) {
+		if ((num.u[i] & mask[i]) != 0) {
+		    return 1;
+		}
+	    }
+	    return 0;
+	}
+#if 0
+	uint32_t next_uint32() {
+	    int p = idx / 4;
+	    if (idx % 4 == 0) {
+		do_recursion();
+		index = (index + 1) % size;
+	    }
+	    uint32_t work = state[p].u[idx % 4];
+	    idx = (idx + 1) % (size * 4);
+	    return work;
+	}
+#endif
+	void seeding(uint32_t seed) {
+	    int i;
+	    uint32_t * psfmt32 = &state[0].u[0];
+
+	    psfmt32[0] = seed;
+	    for (i = 1; i < size * 4; i++) {
+		psfmt32[i] = UINT32_C(1812433253)
+		    * (psfmt32[i - 1] ^ (psfmt32[i - 1] >> 30))
+		    + i;
+	    }
+	    idx = 0;
+	    period_certification();
+	}
+    private:
+	int mexp;
+	int maxdegree;
+	int sl1;
+	int sl2;
+	int sr1;
+	int sr2;
+	int pos1;
+	int size;
+	int index;
+	int bitpos;
+	int idx;
+	uint32_t mask1;
+	uint32_t mask2;
+	uint32_t mask3;
+	uint32_t mask4;
+	uint32_t parity[4];
+	w128_t * state;
+
+	void rshift128(w128_t *out, w128_t const *in, int shift) {
+	    uint64_t th, tl, oh, ol;
+
+	    th = ((uint64_t)in->u[3] << 32) | ((uint64_t)in->u[2]);
+	    tl = ((uint64_t)in->u[1] << 32) | ((uint64_t)in->u[0]);
+
+	    oh = th >> (shift * 8);
+	    ol = tl >> (shift * 8);
+	    ol |= th << (64 - shift * 8);
+	    out->u[1] = (uint32_t)(ol >> 32);
+	    out->u[0] = (uint32_t)ol;
+	    out->u[3] = (uint32_t)(oh >> 32);
+	    out->u[2] = (uint32_t)oh;
+	}
+
+	void lshift128(w128_t *out, w128_t const *in, int shift) {
+	    uint64_t th, tl, oh, ol;
+
+	    th = ((uint64_t)in->u[3] << 32) | ((uint64_t)in->u[2]);
+	    tl = ((uint64_t)in->u[1] << 32) | ((uint64_t)in->u[0]);
+
+	    oh = th << (shift * 8);
+	    ol = tl << (shift * 8);
+	    oh |= tl >> (64 - shift * 8);
+	    out->u[1] = (uint32_t)(ol >> 32);
+	    out->u[0] = (uint32_t)ol;
+	    out->u[3] = (uint32_t)(oh >> 32);
+	    out->u[2] = (uint32_t)oh;
+	}
+
+	void do_recursion() {
+	    do_recursion(&state[index],
+			 &state[index],
+			 &state[(index + pos1) % size],
+			 &state[(index + size - 2) % size],
+			 &state[(index + size - 1) % size]);
+	}
+
+	void do_recursion(w128_t *r, w128_t *a, w128_t *b, w128_t *c,
+			  w128_t *d) {
+	    w128_t x;
+	    w128_t y;
+
+	    lshift128(&x, a, sl2);
+	    rshift128(&y, c, sr2);
+	    r->u[0] = a->u[0] ^ x.u[0] ^ ((b->u[0] >> sr1) & mask1) ^ y.u[0]
+		^ (d->u[0] << sl1);
+	    r->u[1] = a->u[1] ^ x.u[1] ^ ((b->u[1] >> sr1) & mask2) ^ y.u[1]
+		^ (d->u[1] << sl1);
+	    r->u[2] = a->u[2] ^ x.u[2] ^ ((b->u[2] >> sr1) & mask3) ^ y.u[2]
+		^ (d->u[2] << sl1);
+	    r->u[3] = a->u[3] ^ x.u[3] ^ ((b->u[3] >> sr1) & mask4) ^ y.u[3]
+		^ (d->u[3] << sl1);
+	}
+
+	void period_certification(void) {
+	    int inner = 0;
+	    int i, j;
+	    uint32_t work;
+	    uint32_t * psfmt32 = &state[0].u[0];
+
+	    for (i = 0; i < 4; i++)
+		inner ^= psfmt32[i] & parity[i];
+	    for (i = 16; i > 0; i >>= 1)
+		inner ^= inner >> i;
+	    inner &= 1;
+	    /* check OK */
+	    if (inner == 1) {
+		return;
+	    }
+	    /* check NG, and modification */
+	    for (i = 0; i < 4; i++) {
+		work = 1;
+		for (j = 0; j < 32; j++) {
+		    if ((work & parity[i]) != 0) {
+			psfmt32[i] ^= work;
+			return;
+		    }
+		    work = work << 1;
+		}
+	    }
+	}
+    };
+}
+#endif
---
+++ release/SFMT/jump/calc-characteristic.cpp
@@ -0,0 +1,240 @@
+/**
+ * @file calc-characteristic.cpp
+ *
+ * @brief calculate characteristic polynomial.
+ *
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (The University of Tokyo)
+ *
+ * Copyright (C) 2012 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and The University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <inttypes.h>
+#include <stdint.h>
+#include "SFMText.hpp"
+#include <NTL/GF2X.h>
+#include <NTL/vec_GF2.h>
+#include <NTL/GF2XFactoring.h>
+
+using namespace sfmt;
+using namespace NTL;
+using namespace std;
+
+void calc_minimal(GF2X& minimal, SFMText& sfmt, int bitpos)
+{
+    uint32_t mask[4];
+    for (int i = 0; i < 4; i++) {
+	mask[i] = 0;
+    }
+    int pos = bitpos / 32;
+    uint32_t m = 1 << (bitpos % 32);
+    mask[pos] = m;
+    int maxdegree = sfmt.get_maxdegree();
+    vec_GF2 seq;
+    seq.SetLength(2 * maxdegree);
+    for (int i = 0; i < 2 * maxdegree; i++) {
+	seq[i] = sfmt.next(mask);
+    }
+    MinPolySeq(minimal, seq, maxdegree);
+#ifdef DEBUG
+    if (deg(minimal) == 0) {
+	cout << "deg minimal:" << dec << deg(minimal) << endl;
+	cout << "minimal:" << minimal << endl;
+	cout << "seq:" << seq << endl;
+    }
+#endif
+}
+
+void LCM(GF2X& lcm, const GF2X& x, const GF2X& y) {
+    GF2X gcd;
+    mul(lcm, x, y);
+    GCD(gcd, x, y);
+    lcm /= gcd;
+}
+
+void get_lcm(GF2X& lcmpoly, SFMText& sfmt) {
+    GF2X minimal;
+    GF2X tmp;
+    int maxdegree = sfmt.get_maxdegree();
+    sfmt.seeding(1234);
+    for (int bitpos = 0; bitpos < 128; bitpos++) {
+	calc_minimal(minimal, sfmt, bitpos);
+	LCM(tmp, lcmpoly, minimal);
+	lcmpoly = tmp;
+	if (deg(lcmpoly) == maxdegree) {
+	    return;
+	}
+    }
+    for (int i = 0; i < maxdegree; i++) {
+	sfmt.init_basis();
+	for (int bitpos = 0; bitpos < 128; bitpos++) {
+	    calc_minimal(minimal, sfmt, bitpos);
+	    LCM(tmp, lcmpoly, minimal);
+	    lcmpoly = tmp;
+	    if (deg(lcmpoly) == maxdegree) {
+		return;
+	    }
+	}
+    }
+    cerr << "deg:" << deg(lcmpoly) << endl;
+    throw new logic_error("can't find lcm");
+}
+
+void check(SFMText& sfmt) {
+    sfmt.seeding(1234);
+
+    for (int i = 0; i < 10; i++) {
+	w128_t x = sfmt.next();
+	for (int j = 0; j < 4; j++) {
+	    cout << dec << x.u[j] << endl;
+	}
+    }
+}
+
+int non_reducible(GF2X& fpoly, int degree) {
+    static const GF2X t2(2, 1);
+    static const GF2X t1(1, 1);
+    GF2X t2m;
+    GF2X t;
+    GF2X alpha;
+    int m;
+
+    t2m = t2;
+    if (deg(fpoly) < degree) {
+	return 0;
+    }
+    t = t1;
+    t += t2m;
+
+    for (m = 1; deg(fpoly) > degree; m++) {
+	for(;;) {
+	    GCD(alpha, fpoly, t);
+	    if (IsOne(alpha)) {
+		break;
+	    }
+	    fpoly /= alpha;
+	    if (deg(fpoly) < degree) {
+		return 0;
+	    }
+	}
+	t2m *= t2m;
+	t2m %= fpoly;
+	add(t, t2m, t1);
+    }
+    if (deg(fpoly) != degree) {
+	return 0;
+    }
+    return IterIrredTest(fpoly);
+}
+
+void polytostring(string& x, GF2X& characteristic) {
+    long degree = deg(characteristic);
+    int size = degree / 64 + 1;
+    uint64_t buffer[size];
+    for (int i = 0; i < size; i++) {
+	buffer[i] = 0;
+    }
+    for (int i = 0; i <= degree; i++) {
+	int pos = i / 64;
+	int shift = i % 64;
+	if (IsOne(coeff(characteristic, i))) {
+	    buffer[pos] |= UINT64_C(1) << shift;
+	}
+    }
+    ostringstream os;
+//    os << hex << setw(16);
+//    cout << "buffer[0]:" << hex << setw(16) << setfill('0')
+//	 << buffer[0] << endl;
+    for (int i = 0; i < size; i++) {
+	os << hex << setw(16) << setfill('0') << buffer[i];
+    }
+    os << flush;
+    x = os.str();
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 11) {
+	cout << argv[0]
+	     << "mexp sl1 sl2 sr1 sr2 pos1 mask1 mask2 mask3 mask4"
+	     << " parity1 parity2 parity3 parity4"
+	     << endl;
+	return -1;
+    }
+    int mexp = strtol(argv[1], NULL, 10);
+    int pos1 = strtol(argv[2], NULL, 10);
+    int sl1 = strtol(argv[3], NULL, 10);
+    int sl2 = strtol(argv[4], NULL, 10);
+    int sr1 = strtol(argv[5], NULL, 10);
+    int sr2 = strtol(argv[6], NULL, 10);
+    uint32_t mask[4];
+    mask[0] = strtoull(argv[7], NULL, 16);
+    mask[1] = strtoull(argv[8], NULL, 16);
+    mask[2] = strtoull(argv[9], NULL, 16);
+    mask[3] = strtoull(argv[10], NULL, 16);
+    uint32_t parity[4];
+    parity[0] = strtoull(argv[11], NULL, 16);
+    parity[1] = strtoull(argv[12], NULL, 16);
+    parity[2] = strtoull(argv[13], NULL, 16);
+    parity[3] = strtoull(argv[14], NULL, 16);
+#if defined(DEBUG)
+    cout << "mexp:" << dec << mexp << endl;
+    cout << "pos1:" << dec << pos1 << endl;
+    cout << "sl1:" << dec << sl1 << endl;
+    cout << "sl2:" << dec << sl2 << endl;
+    cout << "sr1:" << dec << sr1 << endl;
+    cout << "sr2:" << dec << sr2 << endl;
+    cout << "mask1:" << hex<< mask[0] << endl;
+    cout << "mask2:" << hex << mask[1] << endl;
+    cout << "mask3:" << hex << mask[2] << endl;
+    cout << "mask4:" << hex << mask[3] << endl;
+    cout << "parity1:" << hex<< parity[0] << endl;
+    cout << "parity2:" << hex << parity[1] << endl;
+    cout << "parity3:" << hex << parity[2] << endl;
+    cout << "parity4:" << hex << parity[3] << endl;
+#endif
+    SFMText sfmt(mexp, sl1, sl2, sr1, sr2, pos1,
+		  mask, parity);
+    GF2X characteristic(0, 1);
+#if defined(DEBUG)
+    check(sfmt);
+#endif
+    get_lcm(characteristic, sfmt);
+    GF2X work = characteristic;
+#if defined(DEBUG)
+    cout << "degree:" << deg(characteristic) << endl;
+    cout << characteristic << endl;
+#endif
+    if (!non_reducible(characteristic, mexp)) {
+	cout << "error?" << endl;
+    }
+    string x;
+    polytostring(x, work);
+#if defined(DEBUG)
+    cout << "characteristic:" << x << endl;
+#endif
+    cout << x;
+    cout << "," << dec << mexp;
+    cout << "," << dec << pos1;
+    cout << "," << dec << sl1;
+    cout << "," << dec << sl2;
+    cout << "," << dec << sr1;
+    cout << "," << dec << sr2;
+    cout << "," << hex<< mask[0];
+    cout << "," << hex << mask[1];
+    cout << "," << hex << mask[2];
+    cout << "," << hex << mask[3];
+    cout << "," << hex<< parity[0];
+    cout << "," << hex << parity[1];
+    cout << "," << hex << parity[2];
+    cout << "," << hex << parity[3];
+    cout << endl;
+    return 0;
+}
