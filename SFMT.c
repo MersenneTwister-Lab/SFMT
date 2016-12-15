MersenneTwister-Lab/SFMT

commit 5f3e0cd74826f7a0c104aa3235d03ba0aa37cf33

Add ARM (with NEON) Support
Support ARMv7A with NEON, and ARMv8A(AArch64).
Compiled on Ubuntu 14.04 LTS x64.
Checked on qemu-arm/qemu-arch64.

SFMT.c
@@ -1,4 +1,8 @@
 /**
+ * ARM Port of SFMT
+ * Copyright (C) 2016 Masaki Ota. All rights reserved.
+ */
+/**
  * @file  SFMT.c
  * @brief SIMD oriented Fast Mersenne Twister(SFMT)
  *
@@ -16,7 +20,7 @@
  * The 3-clause BSD License is applied to this software, see
  * LICENSE.txt
  */
-
+ 
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -40,11 +44,6 @@ extern "C" {
 #undef ONLY64
 #endif
 
-/**
- * parameters used by sse2.
- */
-static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
-                                        SFMT_MSK3, SFMT_MSK4}};
 /*----------------
   STATIC FUNCTIONS
   ----------------*/
@@ -65,6 +64,13 @@ inline static void swap(w128_t *array, int size);
   #else
     #include "SFMT-sse2.h"
   #endif
+/**
+ * parameters used by sse2.
+ */
+static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
+                                        SFMT_MSK3, SFMT_MSK4}};
+#elif defined(HAVE_NEON)
+  #include "SFMT-neon.h"
 #endif
 
 /**
@@ -81,7 +87,7 @@ inline static int idxof(int i) {
 }
 #endif
 
-#if (!defined(HAVE_ALTIVEC)) && (!defined(HAVE_SSE2))
+#if (!defined(HAVE_ALTIVEC)) && (!defined(HAVE_SSE2)) && (!defined(HAVE_NEON))
 /**
  * This function fills the user-specified array with pseudorandom
  * integers.
@@ -232,7 +238,7 @@ int sfmt_get_min_array_size64(sfmt_t * sfmt) {
     return SFMT_N64;
 }
 
-#if !defined(HAVE_SSE2) && !defined(HAVE_ALTIVEC)
+#if !defined(HAVE_SSE2) && !defined(HAVE_ALTIVEC) && !defined(HAVE_NEON)
 /**
  * This function fills the internal state array with pseudorandom
  * integers.

Makefile.aarch64
@@ -1,0 +1,116 @@
+# @file  Makefile.aarch64
+# @brief Makefile.aarch64
+#
+# @author Masaki Ota
+#
+# Copyright (C) 2016, Masaki Ota. All rights reserved.
+#
+# The new BSD License is applied to this software.
+# see LICENSE.txt
+#
+# @note
+# We could compile test-neon-Mxxx using gcc-4.8(aarch64) of Linux.
+# 
+WARN = -Wmissing-prototypes -Wall -Winline
+OPTI = -O3 -finline-functions -fomit-frame-pointer -DNDEBUG \
+-fno-strict-aliasing --param max-inline-insns-single=1800
+STD = -std=c99
+CC = aarch64-linux-gnu-gcc
+CCFLAGS = $(OPTI) $(WARN) $(STD)
+NEONFLAGS = -DHAVE_NEON -ftree-vectorize
+ARM_TARGET = test-arm-M19937
+NEON_TARGET = test-neon-M19937
+
+ALL_ARM_TARGET = test-arm-M607 test-arm-M1279 test-arm-M2281 test-arm-M4253 \
+test-arm-M11213 test-arm-M19937 test-arm-M44497 test-arm-M86243 \
+test-arm-M132049 test-arm-M216091
+
+ALL_NEON_TARGET = test-neon-M607 test-neon-M1279 test-neon-M2281 \
+test-neon-M4253 test-neon-M11213 test-neon-M19937 test-neon-M44497 \
+test-neon-M86243 test-neon-M132049 test-neon-M216091
+
+.PHONY: arm-check neon-check
+
+arm: $(ARM_TARGET)
+
+neon:$(NEON_TARGET)
+
+
+arm-check: $(ALL_ARM_TARGET)
+	./check.sh 32 test-arm
+
+neon-check: $(ALL_NEON_TARGET)
+	./check.sh 32 test-neon
+
+test-arm-M607: test.c SFMT.c SFMT.h SFMT-params607.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=607 -o $@ test.c SFMT.c
+
+test-neon-M607: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params607.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=607 -o $@ test.c SFMT.c
+
+test-arm-M1279: test.c SFMT.c SFMT.h SFMT-params1279.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=1279 -o $@ test.c SFMT.c
+
+test-neon-M1279: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params1279.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=1279 -o $@ test.c SFMT.c
+
+test-arm-M2281: test.c SFMT.c SFMT.h SFMT-params2281.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=2281 -o $@ test.c SFMT.c
+
+test-neon-M2281: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params2281.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=2281 -o $@ test.c SFMT.c
+
+test-arm-M4253: test.c SFMT.c SFMT.h SFMT-params4253.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=4253 -o $@ test.c SFMT.c
+
+test-neon-M4253: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params4253.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=4253 -o $@ test.c SFMT.c
+
+test-arm-M11213: test.c SFMT.c SFMT.h SFMT-params11213.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=11213 -o $@ test.c SFMT.c
+
+test-neon-M11213: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params11213.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=11213 -o $@ test.c SFMT.c
+
+test-arm-M19937: test.c SFMT.c SFMT.h SFMT-params19937.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=19937 -o $@ test.c SFMT.c
+
+test-neon-M19937: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params19937.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=19937 -o $@ test.c SFMT.c
+
+asm-neon-M19937: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params19937.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=19937 -s $@ test.c SFMT.c -S
+
+test-arm-M44497: test.c SFMT.c SFMT.h SFMT-params44497.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=44497 -o $@ test.c SFMT.c
+
+test-neon-M44497: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params44497.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=44497 -o $@ test.c SFMT.c
+
+test-arm-M86243: test.c SFMT.c SFMT.h SFMT-params86243.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=86243 -o $@ test.c SFMT.c
+
+test-neon-M86243: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params86243.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=86243 -o $@ test.c SFMT.c
+
+test-arm-M132049: test.c SFMT.c SFMT.h SFMT-params132049.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=132049 -o $@ test.c SFMT.c
+
+test-neon-M132049: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params132049.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=132049 -o $@ test.c SFMT.c
+
+test-arm-M216091: test.c SFMT.c SFMT.h SFMT-params216091.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=216091 -o $@ test.c SFMT.c
+
+test-neon-M216091: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params216091.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=216091 -o $@ test.c SFMT.c
+
+clean:
+	rm -f *.o *~

Makefile.armv7a
@@ -1,0 +1,118 @@
+# @file  Makefile.armv7a
+# @brief Makefile.armv7a
+#
+# @author Masaki Ota
+#
+# Copyright (C) 2016, Masaki Ota. All rights reserved.
+#
+# The new BSD License is applied to this software.
+# see LICENSE.txt
+#
+# @note
+# We could compile test-neon-Mxxx using gcc-4.8(armv7a) of Linux.
+# 
+
+WARN = -Wmissing-prototypes -Wall -Winline
+OPTI = -O3 -finline-functions -fomit-frame-pointer -DNDEBUG \
+-fno-strict-aliasing --param max-inline-insns-single=1800
+STD = -std=c99
+CC = arm-linux-gnueabihf-gcc-4.8
+CCFLAGS = $(OPTI) $(WARN) $(STD)
+NEONFLAGS = -DHAVE_NEON -mfpu=neon -ftree-vectorize
+ARM_TARGET = test-arm-M19937
+NEON_TARGET = test-neon-M19937
+
+ALL_ARM_TARGET = test-arm-M607 test-arm-M1279 test-arm-M2281 test-arm-M4253 \
+test-arm-M11213 test-arm-M19937 test-arm-M44497 test-arm-M86243 \
+test-arm-M132049 test-arm-M216091
+
+ALL_NEON_TARGET = test-neon-M607 test-neon-M1279 test-neon-M2281 \
+test-neon-M4253 test-neon-M11213 test-neon-M19937 test-neon-M44497 \
+test-neon-M86243 test-neon-M132049 test-neon-M216091
+
+.PHONY: arm-check neon-check
+
+arm: $(ARM_TARGET)
+
+neon:$(NEON_TARGET)
+
+
+arm-check: $(ALL_arm_TARGET)
+	./check.sh 32 test-arm
+
+neon-check: $(ALL_NEON_TARGET)
+	./check.sh 32 test-neon
+
+test-arm-M607: test.c SFMT.c SFMT.h SFMT-params607.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=607 -o $@ test.c SFMT.c
+
+test-neon-M607: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params607.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=607 -o $@ test.c SFMT.c
+
+test-arm-M1279: test.c SFMT.c SFMT.h SFMT-params1279.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=1279 -o $@ test.c SFMT.c
+
+test-neon-M1279: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params1279.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=1279 -o $@ test.c SFMT.c
+
+test-arm-M2281: test.c SFMT.c SFMT.h SFMT-params2281.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=2281 -o $@ test.c SFMT.c
+
+test-neon-M2281: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params2281.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=2281 -o $@ test.c SFMT.c
+
+test-arm-M4253: test.c SFMT.c SFMT.h SFMT-params4253.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=4253 -o $@ test.c SFMT.c
+
+test-neon-M4253: test.c SFMT.c SFMT.h SFMT-neon.h SFMT-params4253.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=4253 -o $@ test.c SFMT.c
+
+test-arm-M11213: test.c SFMT.c SFMT.h SFMT-params11213.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=11213 -o $@ test.c SFMT.c
+
+test-neon-M11213: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params11213.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=11213 -o $@ test.c SFMT.c
+
+test-arm-M19937: test.c SFMT.c SFMT.h SFMT-params19937.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=19937 -o $@ test.c SFMT.c
+
+test-neon-M19937: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params19937.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=19937 -o $@ test.c SFMT.c
+
+asm-neon-M19937: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params19937.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=19937 -s $@ test.c SFMT.c -S
+
+test-arm-M44497: test.c SFMT.c SFMT.h SFMT-params44497.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=44497 -o $@ test.c SFMT.c
+
+test-neon-M44497: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params44497.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=44497 -o $@ test.c SFMT.c
+
+test-arm-M86243: test.c SFMT.c SFMT.h SFMT-params86243.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=86243 -o $@ test.c SFMT.c
+
+test-neon-M86243: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params86243.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=86243 -o $@ test.c SFMT.c
+
+test-arm-M132049: test.c SFMT.c SFMT.h SFMT-params132049.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=132049 -o $@ test.c SFMT.c
+
+test-neon-M132049: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params132049.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=132049 -o $@ test.c SFMT.c
+
+test-arm-M216091: test.c SFMT.c SFMT.h SFMT-params216091.h
+	$(CC) $(CCFLAGS) -DSFMT_MEXP=216091 -o $@ test.c SFMT.c
+
+test-neon-M216091: test.c SFMT.c SFMT.h SFMT-neon.h \
+	SFMT-params216091.h
+	$(CC) $(CCFLAGS) $(NEONFLAGS) -DSFMT_MEXP=216091 -o $@ test.c SFMT.c
+
+
+clean:
+	rm -f *.o *~

Makefile.me
@@ -4,6 +4,7 @@ FILES = SFMT.h \
 	SFMT-params.h \
 	SFMT.c \
 	SFMT-alti.h \
+	SFMT-neon.h \
 	SFMT-sse2.h \
 	SFMT-sse2-msc.h \
 	SFMT-common.h \
@@ -30,14 +31,16 @@ FILES = SFMT.h \
 	FILES.txt \
 	LICENSE.txt \
 	Makefile \
+	Makefile.armv7a \
+	Makefile.aarch64 \
 	test.c \
 	check.sh \
 	CHANGE-LOG.txt \
 	README.txt \
 	html
 
-VERSION = 1.4.1
-DIR = SFMT-src-${VERSION}
+VERSION = 1.4.1r1
+DIR = SFMT-armport-src-${VERSION}
 
 doc:
 	rm -rf html

SFMT-neon.h
@@ -1,0 +1,139 @@
+/*****************************************************************************
+Copyright (c) 2016 Masaki Ota. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the names of Hiroshima University, The University of
+      Tokyo nor the names of its contributors may be used to endorse
+      or promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+/**
+ * @file  SFMT-neon.h
+ * @brief SIMD oriented Fast Mersenne Twister(SFMT) for ARM with 128b NEON 
+ *
+ * @note We assume LITTLE ENDIAN in this file
+ */
+
+#ifndef SFMT_NEON_H
+#define SFMT_NEON_H
+
+inline static void neon_recursion(uint32x4_t * r, uint32x4_t a, uint32x4_t b,
+                                uint32x4_t c, uint32x4_t d);
+
+
+
+/**
+ * This function represents the recursion formula.
+ * @param r an output
+ * @param a a 128-bit part of the interal state array
+ * @param b a 128-bit part of the interal state array
+ * @param c a 128-bit part of the interal state array
+ * @param d a 128-bit part of the interal state array
+ */
+inline static void neon_recursion(uint32x4_t * r, uint32x4_t a, uint32x4_t b,
+                                uint32x4_t c, uint32x4_t d)
+{
+    uint32x4_t v, x, y, z;
+    static const uint32x4_t vzero = {0,0,0,0};
+    static const uint32x4_t vmask = {SFMT_MSK1, SFMT_MSK2, SFMT_MSK3, SFMT_MSK4};
+
+#define rotate_bytes(A, B, C) vreinterpretq_u32_u8(vextq_u8(vreinterpretq_u8_u32(A),vreinterpretq_u8_u32(B),(C)))
+
+    y = vshrq_n_u32(b, SFMT_SR1);
+    z = rotate_bytes(c, vzero, SFMT_SR2);
+    v = vshlq_n_u32(d, SFMT_SL1);
+    z = veorq_u32(z, a);
+    z = veorq_u32(z, v);
+    x = rotate_bytes(vzero, a, 16-SFMT_SL2);
+    y = vandq_u32(y, vmask);
+    z = veorq_u32(z, x);
+    z = veorq_u32(z, y);
+    *r = z;
+}
+
+/**
+ * This function fills the internal state array with pseudorandom
+ * integers.
+ * @param sfmt SFMT internal state
+ */
+void sfmt_gen_rand_all(sfmt_t * sfmt) {
+    int i;
+    uint32x4_t r1, r2;
+    w128_t * pstate = sfmt->state;
+
+    r1 = pstate[SFMT_N - 2].si;
+    r2 = pstate[SFMT_N - 1].si;
+    for (i = 0; i < SFMT_N - SFMT_POS1; i++) {
+        neon_recursion(&pstate[i].si, pstate[i].si, pstate[i + SFMT_POS1].si, r1, r2);
+        r1 = r2;
+        r2 = pstate[i].si;
+    }
+    for (; i < SFMT_N; i++) {
+        neon_recursion(&pstate[i].si, pstate[i].si, pstate[i + SFMT_POS1 - SFMT_N].si, r1, r2);
+        r1 = r2;
+        r2 = pstate[i].si;
+    }
+}
+
+/**
+ * This function fills the user-specified array with pseudorandom
+ * integers.
+ * @param sfmt SFMT internal state.
+ * @param array an 128-bit array to be filled by pseudorandom numbers.
+ * @param size number of 128-bit pseudorandom numbers to be generated.
+ */
+static void gen_rand_array(sfmt_t * sfmt, w128_t * array, int size)
+{
+    int i, j;
+    uint32x4_t r1, r2;
+    w128_t * pstate = sfmt->state;
+
+    r1 = pstate[SFMT_N - 2].si;
+    r2 = pstate[SFMT_N - 1].si;
+    for (i = 0; i < SFMT_N - SFMT_POS1; i++) {
+        neon_recursion(&array[i].si, pstate[i].si, pstate[i + SFMT_POS1].si, r1, r2);
+        r1 = r2;
+        r2 = array[i].si;
+    }
+    for (; i < SFMT_N; i++) {
+        neon_recursion(&array[i].si, pstate[i].si, array[i + SFMT_POS1 - SFMT_N].si, r1, r2);
+        r1 = r2;
+        r2 = array[i].si;
+    }
+    for (; i < size - SFMT_N; i++) {
+        neon_recursion(&array[i].si, array[i - SFMT_N].si, array[i + SFMT_POS1 - SFMT_N].si, r1, r2);
+        r1 = r2;
+        r2 = array[i].si;
+    }
+    for (j = 0; j < 2 * SFMT_N - size; j++) {
+        pstate[j] = array[j + size - SFMT_N];
+    }
+    for (; i < size; i++, j++) {
+        neon_recursion(&array[i].si, array[i - SFMT_N].si, array[i + SFMT_POS1 - SFMT_N].si, r1, r2);
+        r1 = r2;
+        r2 = pstate[j].si = array[i].si;
+    }
+}
+
+#endif

SFMT.h
@@ -1,4 +1,8 @@
 #pragma once
+/*
+ * ARM Port of SFMT
+ * Copyright (C) 2016 Masaki Ota. All rights reserved.
+ */
 /**
  * @file SFMT.h
  *
@@ -78,6 +82,15 @@ union W128_T {
     vector unsigned int s;
     uint32_t u[4];
     uint64_t u64[2];
+};
+#elif defined(HAVE_NEON)
+  #include <arm_neon.h>
+
+/** 128-bit data structure */
+union W128_T {
+    uint32_t u[4];
+    uint64_t u64[2];
+    uint32x4_t si;
 };
 #elif defined(HAVE_SSE2)
   #include <emmintrin.h>

commit 89ae2de69709aeec179bcf8543797c2f21493334

Fix a minor bug introduced by 5f3e0cd74826f7a0c104aa3235d03ba0aa37cf33

SFMT.c
@@ -16,7 +16,7 @@
  * The 3-clause BSD License is applied to this software, see
  * LICENSE.txt
  */
- 
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -55,16 +55,16 @@ inline static void swap(w128_t *array, int size);
 #if defined(HAVE_ALTIVEC)
   #include "SFMT-alti.h"
 #elif defined(HAVE_SSE2)
+/**
+ * parameters used by sse2.
+ */
+  static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
+                                          SFMT_MSK3, SFMT_MSK4}};
   #if defined(_MSC_VER)
     #include "SFMT-sse2-msc.h"
   #else
     #include "SFMT-sse2.h"
   #endif
-/**
- * parameters used by sse2.
- */
-static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
-                                        SFMT_MSK3, SFMT_MSK4}};
 #elif defined(HAVE_NEON)
   #include "SFMT-neon.h"
 #endif